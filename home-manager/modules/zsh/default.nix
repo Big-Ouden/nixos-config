c758420d6ad139275a54e24308524112ed68dfb3
BigOuden <simon.belier29@gmail.com>
2025-02-18 20:47:11 +0100

    update p10k config

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{pkgs, ... }:
 {
   imports = [
     ./zsh.nix
     ./zsh_alias.nix
-    ./zsh_keybinds.nix
+    # ./zsh_keybinds.nix
+    # ./p10k
   ];
+
+home.packages = (with pkgs; [ zsh-powerlevel10k ]);
+
+
+
 }
--- home-manager/modules/zsh/zsh.nix
+++ home-manager/modules/zsh/zsh.nix
@@ -1,17 +1,33 @@
-{...}:
+{ pkgs,...}:
 {
     programs.zsh = {
         enable = true;
-        autosuggestions.enable = true;
+        autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
+        # promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 
-        zplug = {
+
+        oh-my-zsh = {
             enable = true;
             plugins = [
-                {
-                    name = "";
-                }
+                "git"
+                "copyfile"
+                "copypath"
+                "fzf"
+                "ssh"
+                "tmux"
+                "history"
+                "history-substring-search"
+                "zoxide"
+                "zsh-navigation-tools"
+                "sudo"
+                # "you-should-use"
+
             ];
+            # theme = "powerlevel10k";
         };
-    }
+        initExtra =''
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+        '';
+    };
 }

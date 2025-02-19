2f9919164f0752e5e5cc621b3f885f4b453c37f2
BigOuden <simon.belier29@gmail.com>
2025-02-19 11:58:41 +0100

    remakeing hypr config
    using .conf file instead of .nix config file

--- home-manager/modules/hypr/default.nix
+++ home-manager/modules/hypr/default.nix
@@ -1,11 +1,18 @@
-{ inputs, ... }:
+{ pkgs, inputs, lib,...}:
 {
   imports = [
-    ./hyprland.nix
-    ./config.nix
-    ./hyprlock.nix
-    ./variables.nix
     inputs.hyprland.homeManagerModules.default
   ];
-
- }
+  home.file = {
+    lib.mkMerge [
+      {
+        ".config/hypr/hyprland.conf".source=./hyprland.conf;
+        ".config/hypr/keybinds.conf".source=./keybinds.conf;
+        ".config/hypr/rules.conf".source=./rules.conf;
+        ".config/hypr/setup.conf".source=./setup.conf;
+        ".config/hypr/startup.conf".source=./startup.conf;
+        ".config/hypr/colors.conf".source=./colors.conf;
+      }
+    ];
+  };
+}

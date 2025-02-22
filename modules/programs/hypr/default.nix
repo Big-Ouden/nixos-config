996fb95fb68ce63582ad37be0ddb4cb377c6e9d3
bigouden <simon.belier29@gmail.com>
2025-02-22 01:37:30 +0100

    Imports conf file and install necessary package for hypr

--- modules/programs/hypr/default.nix
+++ modules/programs/hypr/default.nix
@@ -1,20 +1,40 @@
-{ pkgs, inputs, lib,...}:
+{
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
 {
   imports = [
     ./variables.nix
+    ./hyprlock.nix
     inputs.hyprland.homeManagerModules.default
-    
+
   ];
-  home.file = 
-    lib.mkMerge  [
-      {
-        ".config/hypr/hyprland.conf".source=./hyprland.conf;
-        ".config/hypr/keybinds.conf".source=./keybinds.conf;
-        ".config/hypr/rules.conf".source=./rules.conf;
-        ".config/hypr/setup.conf".source=./setup.conf;
-        ".config/hypr/startup.conf".source=./startup.conf;
-        ".config/hypr/colors.conf".source=./colors.conf;
-      }
-    ];
-  
+  home.file = lib.mkMerge [
+    {
+      ".config/hypr/hyprland.conf".source = ./hyprland.conf;
+      ".config/hypr/keybinds.conf".source = ./keybinds.conf;
+      ".config/hypr/rules.conf".source = ./rules.conf;
+      ".config/hypr/setup.conf".source = ./setup.conf;
+      ".config/hypr/startup.conf".source = ./startup.conf;
+      ".config/hypr/colors.conf".source = ./colors.conf;
+    }
+  ];
+
+  home.packages = with pkgs; [
+    swww
+    inputs.hypr-contrib.packages.${pkgs.system}.grimblast
+    hyprpicker
+    hyprlock
+    inputs.hyprmag.packages.${pkgs.system}.hyprmag
+    grim
+    slurp
+    wl-clip-persist
+    cliphist
+    wf-recorder
+    glib
+    direnv
+  ];
+
 }

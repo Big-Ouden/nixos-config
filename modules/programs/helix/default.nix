48c0be0042f26f22069b9c98b73a00ea88d09e0b
bigouden <simon.belier29@gmail.com>
2025-03-07 09:59:08 +0100

    Add helix config

--- modules/programs/helix/default.nix
+++ modules/programs/helix/default.nix
@@ -6,24 +6,34 @@
 {
     programs.helix = {
       enable = true;
-      settings = {
-        theme = "autumn_night_transparent";
-        editor.cursor-shape = {
-          normal = "block";
-          insert = "bar";
-          select = "underline";
-        };
-      };
-      languages.language = [{
-        name = "nix";
-        auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-      }];
-      themes = {
-        autumn_night_transparent = {
-          "inherits" = "autumn_night";
-          "ui.background" = { };
-        };
-      };
+      # settings = {
+      #   theme = "autumn_night_transparent";
+      #   editor.cursor-shape = {
+      #     normal = "block";
+      #     insert = "bar";
+      #     select = "underline";
+      #   };
+      # };
+      # languages.language = [{
+      #   name = "nix";
+      #   auto-format = true;
+      #   formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      # }];
+      # themes = {
+      #   autumn_night_transparent = {
+      #     "inherits" = "autumn_night";
+      #     "ui.background" = { };
+      #   };
+      # };
+
+
+
+  home.file = lib.mkMerge [
+    {
+      ".config/helix/config.toml".source = ./config.toml;
+      ".config/helix/languages.toml".source = ./languages.toml;
+    }
+  ];
+
     };
 }

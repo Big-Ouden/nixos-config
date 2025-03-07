3777e51cf7fbc2e678819e4b97d91f70764e14fb
bigouden <simon.belier29@gmail.com>
2025-03-07 15:46:26 +0100

    Trying to add copilot

--- modules/programs/nixvim/plugins/copilot-cmp.nix
+++ modules/programs/nixvim/plugins/copilot-cmp.nix
@@ -2,4 +2,20 @@
   programs.nixvim.plugins.copilot-cmp = {
     enable = true;
   };
+  programs.nixvim.plugins.copilot-lua = {
+    enable = true;
+    suggestion = {
+      enabled = false;
+    };
+    panel = {
+      enabled = false;
+    };
+  };
+
+  extraConfigLua = ''
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+  '';
 }
--- modules/programs/nixvim/plugins/copilot-vim.nix
+++ modules/programs/nixvim/plugins/copilot-vim.nix
@@ -1,5 +1,21 @@
 {
-  programs.nixvim.plugins.copilot-vim = {
+  plugins.copilot-cmp = {
     enable = true;
   };
+  plugins.copilot-lua = {
+    enable = true;
+    suggestion = {
+      enabled = false;
+    };
+    panel = {
+      enabled = false;
+    };
+  };
+
+  extraConfigLua = ''
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+  '';
 }
